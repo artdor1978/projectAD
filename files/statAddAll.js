1c4a38e3d3bd3f4da81c19e3f6b3518acad0dc60
артем <artem@MacBook-Pro-artem.local>
2021-01-09 13:59:49 +0200

    some fix bags

--- files/statAddAll.js
+++ files/statAddAll.js
@@ -1,8 +1,17 @@
-const { teamStat } = require("./statAll.js");
-const { gameProgress } = require("./xgWinDrawLose.js");
-const { poissonDist } = require("./poissonDistribution.js");
-const { dutchTeams, urlsIdDutch } = require("./dutchE.js");
-const odds = require("../output/" + "ODDS2021-01-06.json");
+const {
+	teamStat
+} = require("./statAll.js");
+const {
+	gameProgress
+} = require("./xgWinDrawLose.js");
+const {
+	poissonDist
+} = require("./poissonDistribution.js");
+const {
+	dutchTeams,
+	urlsIdDutch
+} = require("./dutchE.js");
+const odds = require("../output/" + "ODDS2021-01-09.json");
 //nameTeam = "Ajax";
 
 //
@@ -18,19 +27,30 @@ const statAddAll = (tArr) => {
 		//console.log("last15:", teamOne.last15, teamTwo.last15);
 		console.log(
 			"0-0:",
-			teamOne.hzero + teamTwo.hzero + "/" + (teamOne.azero + teamTwo.azero)
+			/*(teamOne.hzero + teamTwo.hzero).toFixed(1) + "/" + (teamOne.azero + teamTwo.azero).toFixed(1), "     ",
+			((teamOne.hzero + teamTwo.hzero) / (teamOne.azero + teamTwo.azero)).toFixed(1), "     ",*/
+			(teamOne.hzero / teamOne.azero).toFixed(1), (teamTwo.azero / teamTwo.hzero).toFixed(1)
 		);
 		console.log(
 			"WIN:",
-			gamePrOne.HW + gamePrTwo.HW + "/" + (gamePrOne.AW + gamePrTwo.AW)
+			/*(gamePrOne.HW + gamePrTwo.HW).toFixed(1) + "/" + (gamePrOne.AW + gamePrTwo.AW).toFixed(1), "     ",
+			((gamePrOne.HW + gamePrTwo.HW) / (gamePrOne.AW + gamePrTwo.AW)).toFixed(1), "     ",*/
+			(gamePrOne.HW / gamePrOne.AW).toFixed(1), (gamePrTwo.AW / gamePrTwo.HW).toFixed(1),
+			gamePrOne.HW, gamePrOne.AW, gamePrTwo.AW, gamePrTwo.HW
 		);
 		console.log(
 			"DRAW:",
-			gamePrOne.HD + gamePrTwo.HD + "/" + (gamePrOne.AD + gamePrTwo.AD)
+			/*(gamePrOne.HD + gamePrTwo.HD).toFixed(1) + "/" + (gamePrOne.AD + gamePrTwo.AD).toFixed(1), "     ",
+			((gamePrOne.HD + gamePrTwo.HD) / (gamePrOne.AD + gamePrTwo.AD)).toFixed(1), "     ",*/
+			(gamePrOne.HD / gamePrOne.AD).toFixed(1), (gamePrTwo.AD / gamePrTwo.HD).toFixed(1),
+			gamePrOne.HD, gamePrOne.AD, gamePrTwo.AD, gamePrTwo.HD,
 		);
 		console.log(
 			"LOSE:",
-			gamePrOne.HL + gamePrTwo.HL + "/" + (gamePrOne.AL + gamePrTwo.AL)
+			/*(gamePrOne.HL + gamePrTwo.HL).toFixed(1) + "/" + (gamePrOne.AL + gamePrTwo.AL).toFixed(1), "     ",
+			((gamePrOne.HL + gamePrTwo.HL) / (gamePrOne.AL + gamePrTwo.AL)).toFixed(1), "     ",*/
+			(gamePrOne.HL / gamePrOne.AL).toFixed(1), (gamePrTwo.AL / gamePrTwo.HL).toFixed(1),
+			gamePrOne.HL, gamePrOne.AL, gamePrTwo.AL, gamePrTwo.HL,
 		);
 		console.log("luck:", teamOne.luck, teamTwo.luck);
 	}
@@ -61,7 +81,7 @@ const statAddAll = (tArr) => {
 		console.log("predict:", predictOne.toFixed(2), predictTwo.toFixed(2));
 		//console.log("forecast:", forecastOne.toFixed(2), forecastTwo.toFixed(2));
 		const closest = (counts, goal) =>
-			counts.reduce(function (prev, curr) {
+			counts.reduce(function(prev, curr) {
 				return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
 			});
 		/*const closest = (haystack, needle) => {
@@ -95,19 +115,21 @@ const statAddAll = (tArr) => {
 			over3 = 0,
 			under3 = 0;
 		for (let index = 0; index < monteHome.length; index++) {
-			monteHome[index] > monteAway[index]
-				? win++
-				: monteHome[index] === monteAway[index]
-				? draw++
-				: lose++;
+			monteHome[index] > monteAway[index] ?
+				win++
+				:
+				monteHome[index] === monteAway[index] ?
+				draw++
+				:
+				lose++;
 			/*monteHome[index] + monteAway[index] > 2.5 ? over2++ : under2++;
 		monteHome[index] + monteAway[index] > 1.5 ? over1++ : under1++;
 		monteHome[index] + monteAway[index] > 3.5 ? over3++ : under3++;*/
 		}
 		//console.log(monteHome,monteAway);
 		//console.log(predictOne, predictTwo);
-		/*console.log("prob:", win / 10000, draw / 10000, lose / 10000);
-		console.log(
+		console.log("prob:", win / 10000, draw / 10000, lose / 10000);
+		/*console.log(
 			"odds:",
 			(1 / (win / 10000)).toFixed(2),
 			(1 / (draw / 10000)).toFixed(2),
@@ -118,35 +140,35 @@ const statAddAll = (tArr) => {
 			"value: ",
 			(
 				odds
-					.filter(
-						(odd) =>
-							(odd.homeTeam === dutchTeams[indOne].oddsportal) &
-							(odd.awayTeam === dutchTeams[indTwo].oddsportal)
-					)
-					.map((v) => +v.win) *
-					(win / 10000) -
+				.filter(
+					(odd) =>
+					(odd.homeTeam === dutchTeams[indOne].oddsportal) &
+					(odd.awayTeam === dutchTeams[indTwo].oddsportal)
+				)
+				.map((v) => +v.win) *
+				(win / 10000) -
 				1
 			).toFixed(2),
 			(
 				odds
-					.filter(
-						(odd) =>
-							(odd.homeTeam === dutchTeams[indOne].oddsportal) &
-							(odd.awayTeam === dutchTeams[indTwo].oddsportal)
-					)
-					.map((v) => +v.draw) *
-					(draw / 10000) -
+				.filter(
+					(odd) =>
+					(odd.homeTeam === dutchTeams[indOne].oddsportal) &
+					(odd.awayTeam === dutchTeams[indTwo].oddsportal)
+				)
+				.map((v) => +v.draw) *
+				(draw / 10000) -
 				1
 			).toFixed(2),
 			(
 				odds
-					.filter(
-						(odd) =>
-							(odd.homeTeam === dutchTeams[indOne].oddsportal) &
-							(odd.awayTeam === dutchTeams[indTwo].oddsportal)
-					)
-					.map((v) => +v.lose) *
-					(lose / 10000) -
+				.filter(
+					(odd) =>
+					(odd.homeTeam === dutchTeams[indOne].oddsportal) &
+					(odd.awayTeam === dutchTeams[indTwo].oddsportal)
+				)
+				.map((v) => +v.lose) *
+				(lose / 10000) -
 				1
 			).toFixed(2)
 		);
